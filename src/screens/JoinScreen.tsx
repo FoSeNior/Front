fced16ae4acc9dc06c7b91e96bbeb1353e78ef84
hoont <queenly9@icloud.com>
2025-01-21 06:17:56 +0900

    navigation 작업

---
+++ src/screens/JoinScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text} from "react-native";
+
+function JoinScreen() {
+    return (
+        <>
+            <SafeAreaView>
+                <Text>Join</Text>
+            </SafeAreaView>
+        </>
+    );
+}
+
+export default JoinScreen;
