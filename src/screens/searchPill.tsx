882fd874d76555508121b7d770f33a29a5f1fbad
KyeongYeongDEV <yeong7098@naver.com>
2025-01-23 14:29:24 +0900

    update : 알림 추가시 navigate goback

---
+++ src/screens/searchPill.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text } from "react-native";
+
+const SearchPillScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>알약 검색</Text>
+    </SafeAreaView>
+  );
+}
+
+export default SearchPillScreen;
